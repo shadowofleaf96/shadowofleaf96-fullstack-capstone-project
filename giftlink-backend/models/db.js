e89ee6afbda5565dd14da03b4147decb67f26771
shadowofleaf96 <shadowofleaf96@gmail.com>
2025-04-21 10:11:05 -0400

    chores: create api for Gift Service

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,22 @@
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);      
-
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    if (!dbInstance) {
+        try {
+            await client.connect();
+            dbInstance = client.db(dbName);
+            console.log('✅ Connected to GiftLing DB');
+        } catch (error) {
+            console.error('❌ Failed to connect:', error);
+        }
+    }
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
