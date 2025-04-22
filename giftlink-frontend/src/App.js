f67528cdc90a99e8b99bba5dce8daf84686e527e
Shadow Of Leaf <shadowofleaf96@gmail.com>
2025-04-22 16:15:15 +0100

    fix: remove UNNECESSARY functions

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import LoginPage from "./components/LoginPage/LoginPage"
